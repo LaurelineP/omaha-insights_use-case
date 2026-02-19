0b1a2a9b7b629e615a7904a1f29badb07761290b
Laureline <laurelinedev@gmail.com>
2026-02-19 17:25:45 +0100

    [ feature/dashboard ] Data review - TS to JSON

---
+++ types/data.types.ts
@@ -0,0 +1,20 @@
+export interface CompanyData {
+    customer_id: string;
+    name: string;
+    description: string;
+}
+
+export interface StatData {
+  company_id: number;
+  is_financial: boolean;
+  fiscal_year: number;
+  estimated: boolean;
+  economic_profit: number;
+  rcr_perc_harm: number;
+  RAGR: number;
+}
+
+export interface DashboadData {
+    companies: CompanyData[]
+    stats: StatData[]
+}
